d9a27f5cbef21bb8cfe0f214ec7d62adfe2b654c
ProbirKumare <probirsharma655@gmail.comm>
2024-04-07 14:37:51 +0600

    fake data load

---
+++ src/Components/Shop/Shop.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect( () => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                <h3>Products: {products.length}</h3>
+            </div>
+            <div className="order-container">
+                <h4>Order Summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
