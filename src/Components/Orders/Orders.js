05515061429813182a6d202607d5532172b1954b
ProbirKumare <probirsharma655@gmail.comm>
2024-05-01 10:12:15 +0600

    Remove cart Complate

--- src/Components/Orders/Orders.js
+++ src/Components/Orders/Orders.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
-import { removeFromDb } from '../../utilities/fakedb';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
     const {products, initialCart} = useLoaderData();
@@ -12,6 +12,11 @@ const Orders = () => {
         const remaining = cart.filter(product => product.id !== id);
         setCart(remaining);
         removeFromDb(id);
+    }
+
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
     }
 
     return (
@@ -25,9 +30,12 @@ const Orders = () => {
                             handleRemoveItem={handleRemoveItem}
                         ></ReviewItem>)
                     }
+                    {
+                        cart.length === 0 && <h2>No Item For Review. Please <Link to='/'>Shop More</Link></h2>
+                    }
                 </div>
                 <div className="cart-container">
-                    <Cart cart={cart}></Cart>
+                    <Cart clearCart={clearCart} cart={cart}></Cart>
                 </div>
             </div>
         </div>
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { addToDb, getShoppingCart } from '../../utilities/fakedb';
-import { useLoaderData } from 'react-router-dom';
+import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fakedb';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const products = useLoaderData();
@@ -26,6 +26,12 @@ const Shop = () => {
         }
         setCart(savedCart);
     }, ([products]));
+
+
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
 
     const handleAddToCart = (selectedProduct) =>{
 
@@ -57,7 +63,11 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <Cart cart={cart}></Cart>
+                <Cart clearCart={clearCart} cart={cart}>
+                    <Link to='/orders'>
+                        <button>Review Orders</button>
+                    </Link>
+                </Cart>
             </div>
         </div>
     );
